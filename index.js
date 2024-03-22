6aa998009f87d03354fd095742b143a04676c8cd
oemtv <152803353+oemtv@users.noreply.github.com>
2024-03-22 02:33:13 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let img = ["plain.png", "cap.png", "green-shirt.png", "red.png", "skincare.png", "winter.png"] ;
+let uiterlijk = 0;
+let hamster = document.querySelector("#hamster");
+hamster.src = "images/" + img[uiterlijk] 
+
+function veranderUiterlijk () {
+if (uiterlijk <=4)
+    {uiterlijk += 1;
+}
+else {uiterlijk = 0};
+hamster.src = "images/" + img[uiterlijk] 
+}
+
+hamster.addEventListener ("click", veranderUiterlijk);
+
+document.getElementById('muteButton').addEventListener('click', function() {
+    let audio = document.getElementById('audio');
+    audio.muted = !audio.muted; 
+    this.textContent = audio.muted ? "Unmute" : "Mute";
+});
+
